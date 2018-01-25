1b53d5904a5dbffdc5a70682a2ef8b112a44c573
tonkajohn <tonkajohnisa@gmail.com>
2018-01-25 12:57:00 +0100

    second commit

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,2 +1,7 @@
-// let's go!
-alert('yeah!!!')
+import React from 'react';
+import {render} from 'react-dom';
+import './css/style.css';
+import App from './components/App';
+
+import StorePicker from './components/StorePicker';
+render(<App/>, document.querySelector('#main')); 
